009f87eb2d3e098ce11d56f5257db485aefcefb4
mehul-wpos <mehul@wponlinesupport.com>
2021-10-06 13:17:40 +0530

    header file changes
    add tag

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -14,7 +14,7 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 	<head>
-
+		<div class="test"></div>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
